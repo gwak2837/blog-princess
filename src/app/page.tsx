4c32aa9be49c88f4052e64b7e970b86ee4ab66e3
Taeuk Gwak <gwak2837@kakao.com>
2023-03-05 17:09:54 +0900

    Update packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
+      <input className="p-2 border" />
+      <input className="p-2 border" />
+      <input className="p-2 border" />
+      <button className="p-2 border">제출</button>
+
       <div>hello world</div>
     </main>
   )
